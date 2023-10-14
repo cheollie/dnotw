4c5429d02ae29434e78bf7f18a27b8ee44bd8c5c
Chelsea W. <80985676+cheollie@users.noreply.github.com>
2023-10-14 05:28:51 -0400

    oops

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        "bookmark-purple": "#5267DF",
-        "bookmark-red": "#FA5959",
-        "bookmark-blue": "#242A45",
+        "bookmark-purple": "#2E43BA",//"#5267DF",
+        "bookmark-red": "#11D8BB",//"#FA5959",
+        "bookmark-blue": "#2B9EDD",//"#242A45",
         "bookmark-grey": "#9194A2",
-        "bookmark-white": "#f7f7f7",
+        "bookmark-white": "#060721",//"#f7f7f7",
       },
     },
     fontFamily: {
